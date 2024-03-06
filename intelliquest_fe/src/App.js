5ffb463bc7e7bffdb5028476779d4810e0e22669
ShaktidharK1997 <108393456+ShaktidharK1997@users.noreply.github.com>
2024-03-05 19:36:01 -0500

    Creating a backup

---
+++ intelliquest_fe/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchPage from './components/SearchPage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Academic Paper Search</h1>
+      <SearchPage />
+    </div>
+  );
+}
+
+export default App;
+
